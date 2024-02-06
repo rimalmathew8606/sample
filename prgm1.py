1c3a75df59449addd2807ccc52a3c63981e30b8c
rimal mathew <rimalmathew8606@gmail.com>
2024-02-06 23:56:50 +0530

    added

---
+++ prgm1.py
@@ -0,0 +1,25 @@
+#Q1: average of three numbers 45,67,23
+
+a=45
+b=67
+c=23
+sum=a+b+c
+print(sum)
+average=sum/3
+print(average)
+
+#Q2: Sawp two numbers(using third variable)
+
+a=2
+b=3
+c=a
+a=b
+b=c
+print(a,b)
+
+#Q2: Sawp two numbers( without using third variable)
+
+a=2
+b=3
+a,b=b,a
+print(a,b)
